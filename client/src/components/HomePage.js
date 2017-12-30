d1d5ce43f5f4c35db816ceed537c1e54ada3fa82
Unknown <timpike22@gmail.com>
2017-12-30 14:29:17 -0700

    merging

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -18,7 +18,8 @@ class HomePage extends React.Component {
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h1>Hi {user.firstName}!</h1>
-                <p>You're logged in with React!!</p>
+                <p>Are you looking for a spot or here to rent your spot?</p>
+                
                 <h3>All registered users:</h3>
                 {users.loading && <em>Loading users...</em>}
                 {users.error && <span className="text-danger">ERROR: {users.error}</span>}
