860a376a34a1fbb124146aa31a93cf639ff24211
Unknown <timpike22@gmail.com>
2017-12-29 22:19:02 -0700

    login made

---
+++ client/src/actions/googleMapsParking.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+
+
+const API_KEY = 'AIzaSyDkFM9saBLSb7Vb04SRihSyGC7Eg5YPBdI';
+const ROOT_URL = `https://maps.googleapis.com/maps/api/geocode/json/&key=${API_KEY}`;
+ 
+
+export const FETCH_PARKINGSPACE = 'FETCH_PARKINGSPACE';
+
+
+
+export function fetchParkingSpace(city) {
+    const url = `${ROOT_URL}?${city}`;
+    const request = axios.get(url);
+    return {
+        type: FETCH_PARKINGSPACE,
+        payload: request
+    };
+}
+
+//allow this to take in a latitude and longitude and then spit out the address when searched
