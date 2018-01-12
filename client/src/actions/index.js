004498fb2d1dc1fa0274169c931c135a2127c520
Andrew Presnell <andrew_presnell@hotmail.com>
2018-01-12 16:39:39 -0700

    Update index.js

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,3 +3,5 @@ export * from './parkingSpaceSelected';
 export * from './googleMapsParking';
 export * from './driver-actions';
 export * from './owner-actions';
+export * from './vehicle-actions';
+export * from './parkingSpot-actions';
