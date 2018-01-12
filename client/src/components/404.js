903b7ece8c23e3380063276c1481b323144106c8
Unknown <timpike22@gmail.com>
2018-01-12 16:02:19 -0700

    change

---
+++ client/src/components/404.js
@@ -0,0 +1,15 @@
+"use strict";
+var React = require('react');
+var Link = require('react-router').Link;
+var NotFoundPage = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>Page Not Found</h1>
+                <p>Sorry, there is nothing to see here.</p>
+                <p><Link to="app">Back to Home</Link></p>
+            </div>
+        );
+    }
+});
+module.exports = NotFoundPage;
