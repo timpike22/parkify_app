ddb960fe716b09061c0e0d6568758c028be283aa
Andrew Presnell <andrew_presnell@hotmail.com>
2018-01-12 15:48:50 -0700

    added parking spot contstant

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,3 +1,5 @@
 export * from './alert-constants';
 export * from './owner-constants';
 export * from './driver-constants';
+export * from './vehicle-constants';
+export * from './parkingSpot-constants';
