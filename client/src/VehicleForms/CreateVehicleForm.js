5cd7838a7b627caa6c03548b64d5327e80a18ae0
Andrew Presnell <andrew_presnell@hotmail.com>
2018-01-12 15:39:11 -0700

    Init Vehicle Forms

--- client/src/VehicleForms/CreateVehicleForm.js
+++ client/src/VehicleForms/CreateVehicleForm.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { vehicleService} from '../services'
+import { createSuccess, createFailure } from '../actions/vehicle-actions';
+import { history } from '../helpers';
+import Jumbotron from '../components/Jumbotron';
+import axios from 'axios';
 
-class Vehicle extends React.Component {
-
+class CreateVehicleForm extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            vehice: {
-                driverID: "",
+            vehicle: {
+                driverID: '',
                 make: '',
                 model: '',
                 year: '',
@@ -35,7 +42,7 @@ class Vehicle extends React.Component {
 
     handleChange(event) {
         const { name, value } = event.target;
-        const { user } = this.state;
+        const { vehicle } = this.state;
         this.setState({
             vehicle: {
                 ...vehicle,
@@ -48,8 +55,9 @@ class Vehicle extends React.Component {
         event.preventDefault();
 
         this.setState({ submitted: true });
-        const { user, userType } = this.state;
+        const { vehicle } = this.state;
         const { dispatch } = this.props;
+        console.log(vehicle);
         if (vehicle.driverID && vehicle.make && vehicle.model && vehicle.year && vehicle.color) {
             // TODO: change to the one for the driver
             return vehicleService.create(vehicle).then(response => {
@@ -63,7 +71,7 @@ class Vehicle extends React.Component {
                     dispatch(createFailure())
                     this.setState({
                         vehicle: {
-                            ownerID: '',
+                            driverID: '',
                             make: '',
                             model: '',
                             year: '',
@@ -79,35 +87,30 @@ class Vehicle extends React.Component {
     render() {
         const { creating } = this.props;
         const { vehicle, submitted } = this.state;
+        vehicle.driverID = localStorage.getItem("driver");
+        console.log("state", localStorage.driver);
         return(
             <div className="col-md-6 col-md-offset-3">
                 <Jumbotron>
-                    <h2>Create a Vehicle</h2>
+                    <h2>Add a Vehicle</h2>
                     <form name="createVehicleForm" onSubmit={this.handleSubmit}>
-                        <div className={'form-group' + (submitted && !vehicle.driverID ? ' has-error' : '')}>
-                            <label htmlFor="driverID">Driver ID</label>
-                            <input type="text" className="form-control" name="driverID" value={vehicle.driverID} onChange={this.handleChange} />
-                            {submitted && !vehicle.driverID &&
-                                <div className="help-block">Driver ID is required</div>
-                            }
-                        </div>
                         <div className={'form-group' + (submitted && !vehicle.make ? ' has-error' : '')}>
                             <label htmlFor="vehicleMake">Make</label>
-                            <input type="text" className="form-control" name="vehicleMake" value={vehicle.make} onChange={this.handleChange} />
+                            <input type="text" className="form-control" name="make" value={vehicle.make} onChange={this.handleChange} />
                             {submitted && !vehicle.make &&
                                 <div className="help-block">Vehicle make is required</div>
                             }
                         </div>
                         <div className={'form-group' + (submitted && !vehicle.model ? ' has-error' : '')}>
                             <label htmlFor="vehicleModel">Model</label>
-                            <input type="text" className="form-control" name="vehicleModel" value={vehicle.model} onChange={this.handleChange} />
+                            <input type="text" className="form-control" name="model" value={vehicle.model} onChange={this.handleChange} />
                             {submitted && !vehicle.model &&
                                 <div className="help-block">Vehicle model is required</div>
                             }
                         </div>
                         <div className={'form-group' + (submitted && !vehicle.year ? ' has-error' : '')}>
                             <label htmlFor="vehicleYear">Year</label>
-                            <input type="number" className="form-control" name="vehicleYear" value={vehicle.year} onChange={this.handleChange} />
+                            <input type="number" className="form-control" name="year" value={vehicle.year} onChange={this.handleChange} />
                             {submitted && !vehicle.year &&
                                 <div className="help-block">Vehicle year is required</div>
                             }
@@ -115,16 +118,16 @@ class Vehicle extends React.Component {
                         </div>
                         <div className={'form-group' + (submitted && !vehicle.color ? ' has-error' : '')}>
                             <label htmlFor="vehicleColor">Color</label>
-                            <input type="number" className="form-control" name="vehicleColor" value={vehicle.color} onChange={this.handleChange} />
+                            <input type="text" className="form-control" name="color" value={vehicle.color} onChange={this.handleChange} />
                             {submitted && !vehicle.color &&
                                 <div className="help-block">Vehicle color is required</div>
                             }
 
                         </div>                        
                         <div className="form-group">
-                            <button className="btn btn-primary">Register</button>
+                            <button className="btn btn-primary">Create</button>
 
-                            <Link to="/OwnerLoginPage" className="btn btn-link">Cancel</Link>
+                            <Link to="/DriverHomePage" className="btn btn-link">Cancel</Link>
                         </div>
                     </form>
                 </Jumbotron>    
@@ -134,11 +137,11 @@ class Vehicle extends React.Component {
 }
 
 function mapStateToProps(state) {
-    const { creating } = state.vehicleCreation;
+    const { creating } = state;
     return {
         creating
     };
 }
 
-const connectedVehicle = connect(mapStateToProps)(Vehicle);
-export { connectedVehicle as Vehicle };
+const connectedVehicle = connect(mapStateToProps)(CreateVehicleForm);
+export { connectedVehicle as CreateVehicleForm }; 
