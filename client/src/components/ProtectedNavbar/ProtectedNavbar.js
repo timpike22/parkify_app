7b0122bd5167ef551a4c904eb38b7e0095963efa
Unknown <jmaestas2570@gmail.com>
2018-01-13 00:03:24 -0700

    PNav changes

--- client/src/components/ProtectedNavbar/ProtectedNavbar.js
+++ client/src/components/ProtectedNavbar/ProtectedNavbar.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./ProtectedNavbar.css";
-import logo from "./logo.png"
-
+import logo from "./logo.png";
+import account from "./account.png";
 
 // Depending on the current path, this component sets the "active" className on the appropriate navigation link item
 const PNavbar = props =>
@@ -15,15 +15,24 @@ const PNavbar = props =>
         <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse navbars" id="navbars">
-            <ul className="navbar-nav mr-auto">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
             </ul>
-            <form className="form-inline my-2 my-md-0">
-                <button className="btn btn-warning my-2 owner-login" type="submit">Log Out</button>
+            <form>
+                <div class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown03">
+                        <a class="dropdown-item" href="/settings">Settings</a>
+                        <Link to="/logout" className="btn btn-warning my-2 owner-login" type="submit">Log Out</Link>
+                    </div>
+                </div>
             </form>
+            <img src={account} width="30" height="30" alt="" />
         </div>
     </nav>;
 
     
 
 export default PNavbar;
-
